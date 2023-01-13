4528cac980691b909789fd11014d5dec78efd672
David Pruitt <15479248+davepruitt@users.noreply.github.com>
2023-01-12 21:21:33 -0700

    Initial commit

---
+++ FrameRemoveContentTest/FrameRemoveContentTest/CustomContentView.xaml.cs
@@ -0,0 +1,9 @@
+namespace FrameRemoveContentTest;
+
+public partial class CustomContentView : ContentView
+{
+	public CustomContentView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FrameRemoveContentTest/FrameRemoveContentTest/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace FrameRemoveContentTest;
+
+public partial class MainPage : ContentPage
+{	
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void AddContentButton_Clicked(object sender, EventArgs e)
+    {
+        MyFrame.Content = new CustomContentView();
+    }
+
+    private void RemoveContentButton_Clicked(object sender, EventArgs e)
+    {
+        MyFrame.Content = new ContentView();
+        //MyFrame.Content = null;
+    }
+}
+
